59984691cc857feeee0135cc1092c57f22bb33bb
Edelweiss <edelweiss@murena.io>
2026-05-18 12:02:04 +0200

    fix(multiplestates): handle unique states that have multiple possibilities for three classes

--- server/giny/Sources/Servers/Giny.World/Managers/Fights/Effects/Buffs/AddState.cs
+++ server/giny/Sources/Servers/Giny.World/Managers/Fights/Effects/Buffs/AddState.cs
@@ -1,4 +1,5 @@
 ﻿using Giny.Protocol.Enums;
+using Giny.Protocol.Messages;
 using Giny.World.Managers.Effects;
 using Giny.World.Managers.Fights.Buffs;
 using Giny.World.Managers.Fights.Cast;
@@ -28,7 +29,38 @@ namespace Giny.World.Managers.Fights.Effects.Buffs
 
             foreach (var target in targets)
             {
+                HandleSpecificUniqueState(target, stateRecord);
                 AddStateBuff(target, stateRecord, Effect.DispellableEnum);  
+            }
+        }
+
+        private void HandleSpecificUniqueState(Fighter target, SpellStateRecord stateRecord)
+        {
+            // Dernière rune Hupper
+            if (stateRecord.Id == 701 || stateRecord.Id == 702 || stateRecord.Id == 703 || stateRecord.Id == 704)
+            {
+                target.removeStateBuffFromID(target, 701);
+                target.removeStateBuffFromID(target, 702);
+                target.removeStateBuffFromID(target, 703);
+                target.removeStateBuffFromID(target, 704);
+            }
+
+            // Tromperie Ecaflip
+            if (stateRecord.Id == 579 || stateRecord.Id == 580 || stateRecord.Id == 581 || stateRecord.Id == 582)
+            {
+                target.removeStateBuffFromID(target, 579);
+                target.removeStateBuffFromID(target, 580);
+                target.removeStateBuffFromID(target, 581);
+                target.removeStateBuffFromID(target, 582);
+            }
+
+            // Alchi-Rhetorique Lapino Eniripsa
+            if (stateRecord.Id == 4175 || stateRecord.Id == 4176 || stateRecord.Id == 4177 || stateRecord.Id == 4178)
+            {
+                target.removeStateBuffFromID(target, 4175);
+                target.removeStateBuffFromID(target, 4176);
+                target.removeStateBuffFromID(target, 4177);
+                target.removeStateBuffFromID(target, 4178);
             }
         }
     }
